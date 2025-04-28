798c6066cf7f70ff3a44376377fdecb35a61ebec
Abhayanand Pandey <itsabhayanand@gmail.com>
2025-04-28 20:45:53 +0530

    updated frontend

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,12 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST']
-  }
-});
+
 
 require('dotenv').config();
 connectDB();
